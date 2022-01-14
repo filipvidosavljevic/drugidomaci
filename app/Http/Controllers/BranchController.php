e939ca85f8b3107e8c669594df8fffc55870d47e
vidosavljevic123 <filipvidosavljevic1999@gmail.com>
2022-01-14 02:32:47 +0100

    Kreirane putanje i  kontroler za rad sa ekspoziturom

---
+++ app/Http/Controllers/BranchController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Branch;
+use Illuminate\Http\Request;
+
+class BranchController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        response()->json(Branch::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $b = Branch::create($request->all());
+        return response()->json($b);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Branch  $branch
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Branch $b)
+    {
+        return response()->json($b);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Branch  $branch
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Branch $b)
+    {
+        $b->update($request->all());
+        return response()->json($b);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Branch  $branch
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Branch $branch)
+    {
+        $branch->delete();
+        return response()->noContent();
+    }
+}
